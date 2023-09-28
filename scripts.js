9e5fe4e977c196315e30b15c1fd88a72a32e7c95
Jason Savoie <flimits@gmail.com>
2023-09-27 17:24:27 -0700

    first commit to start coding

---
+++ scripts.js
@@ -0,0 +1,16 @@
+// Let's play a gave of Rock, Paper and Scissors
+
+var playerScores = [,,]; // wins and losses and ties
+var numbers=[0,1,2,3,4,5,6,7,8,9];
+var allowedCharacters = ["r","s","p"];
+var upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz"
+var specialCharacters = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+
+var userResponse = function(){
+   prompt("Enter one of either R, S or P to play ...");
+   if (userResponse.lowerCaseLetters() )
+}
+
+userResponse();
+console.log(userResponse);
