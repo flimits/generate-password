45936b8ff22825f76981fffbfc81b2586c84896d
Jason Savoie <flimits@gmail.com>
2023-09-27 22:44:23 -0700

    finally got one function to work when asking it to get a character to play the game

--- scripts.js
+++ scripts.js
@@ -1,16 +1,31 @@
 // Let's play a gave of Rock, Paper and Scissors
 
-var playerScores = [,,]; // wins and losses and ties
-var numbers=[0,1,2,3,4,5,6,7,8,9];
-var allowedCharacters = ["r","s","p"];
+var playerScores = [, ,]; // wins and losses and ties
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var allowedCharacters = ["r", "s", "p"];
 var upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz"
 var specialCharacters = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+var responseResult = "";
 
-var userResponse = function(){
-   prompt("Enter one of either R, S or P to play ...");
-   if (userResponse.lowerCaseLetters() )
+// function getUserResponse() {
+// var userResponse = prompt("Enter one of either R, S or P to play ...");
+// var lowerCaseResponse = userResponse.tolowerCase();
+
+function getUserResponse(userResponse) {
+
+   var userResp = prompt("Enter one of either R, S or P to play ...");
+   responseResult = userResp.toLowerCase();
+   alert("You entered " + responseResult);
+
+   if (responseResult === "r" || responseResult === "s" || responseResult == "p") {
+      alert("Your entry is ok");
+      // return userResponse;
+   } else {
+      alert("You did not enter an R, S, or P. Please try again");
+      getUserResponse();
+   }
 }
 
-userResponse();
-console.log(userResponse);
+getUserResponse();
+console.log(responseResult);
