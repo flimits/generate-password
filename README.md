7f64bf40a43bd0908f36235ac2230533f542e9a8
Jason Savoie <flimits@gmail.com>
2023-09-28 17:29:08 -0700

    add readme with pseudo code and also started code for project

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # generate-password
 This bit of code will simply prompt user for aspects of a password and then create one for them.
+
+
+## My Pseudo code used when creating the code to create this program.
+
+**I might not have used all of this exactly, but this is the original throught. This is sorta in order..**
+
+* Required to have minimum 8 characters. WE can ask for at least that and if they provide 7, just make it default to 8
+* ask about uppercase
+* ask about lowercase
+* ask about special characters
+* ask about numbers
+* after selections, the generator will crunch them all together in some random order picking some from here and some from there.
+*    If all numbers, or if all of one  type, this should be straight forward to use a random generator to go the a list of that type and pull a character
+*    If there is a mix of two types or three, the generator will generate through which one to use, then go through that type and pull a character
+*    When all characters are generated the meets the length required, exit this function and ...
+* generator will spit out the password in the box.
+* you have the option to continue doing this process
+
+* Create a random function that accepts typd and index required to generate a character. 
+*    ... can it be generic and used by lowercase, uppercase, number and special characters?
+
+* Create a function to ask for size of password
+* Create a function to ask about upper, and then lower, and then special, and then numbers
--- scripts.js
+++ scripts.js
@@ -1,26 +1,35 @@
 // Let's play a gave of Rock, Paper and Scissors
 
-var playerScores = [, ,]; // wins and losses and ties
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-var allowedCharacters = ["r", "s", "p"];
 var upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz"
 var specialCharacters = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
 var responseResult = "";
 
-// function getUserResponse() {
-// var userResponse = prompt("Enter one of either R, S or P to play ...");
-// var lowerCaseResponse = userResponse.tolowerCase();
+
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 function getUserResponse(userResponse) {
 
    var userResp = prompt("Enter one of either R, S or P to play ...");
    responseResult = userResp.toLowerCase();
-   alert("You entered " + responseResult);
+   // alert("You entered " + responseResult);
 
    if (responseResult === "r" || responseResult === "s" || responseResult == "p") {
-      alert("Your entry is ok");
-      // return userResponse;
+      console.log(responseResult + " is a good entry is ok");
    } else {
       alert("You did not enter an R, S, or P. Please try again");
       getUserResponse();
@@ -28,4 +37,23 @@ function getUserResponse(userResponse) {
 }
 
 getUserResponse();
-console.log(responseResult);
+// console.log(responseResult);
+
+
+// Required to have minimum 8 characters. WE can ask for at least that and if they provide 7, just make it default to 8
+// ask about uppercase
+// ask about lowercase
+// ask about special characters
+// ask about numbers
+// after selections, the generator will crunch them all together in some random order picking some from here and some from tehre.
+//    If all numbers, or if all of one  type, this should be straight forward to use a random generator to go the a list of that type and pull a character
+//    If there is a mix of two types or three, the generator will generate through which one to use, then go through that type and pull a character
+//    When all characters are generated the meets the length required, exit this function and ...
+// generator will spit out the password in the box.
+// you have the option to continue doing this process
+
+// Create a random function that accepts typd and index required to generate a character. 
+//    ... can it be generic and used by lowercase, uppercase, number and special characters?
+
+// Create a function to ask for size of password
+// Create a function to ask about upper, and then lower, and then special, and then numbers
